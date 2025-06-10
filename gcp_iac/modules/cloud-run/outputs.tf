dc8466756893f1f3befc36f5007451b9381cf717
Md Abdul Mutalab Shaykat <shaykat2057@gmail.com>
2025-06-10 06:38:47 +0000

    updated the iam policies and service account for cloud run

--- gcp_iac/modules/cloud-run/outputs.tf
+++ gcp_iac/modules/cloud-run/outputs.tf
@@ -2,3 +2,7 @@ output "cloud_run_url" {
   value       = google_cloud_run_v2_service.default.uri
   description = "The URL of the Cloud Run service."
 }
+
+output "name" {
+  value = google_cloud_run_v2_service.default.name
+}
